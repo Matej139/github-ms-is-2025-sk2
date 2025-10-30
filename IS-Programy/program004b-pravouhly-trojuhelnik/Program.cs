30c61c5f7f411e1bbc11170bc7bdb68c0dfa7861
Matěj Svoboda <matej.svoboda@tul.cz>
2025-10-30 11:56:46 +0100

    #6 DU - Vykresleni pravouhleho trojuhelniku

---
+++ IS-Programy/program004b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,35 @@
+﻿//Vykresleni pravoúhlého trojúhelníku hvězdičkami podle zadané výšky.
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Vykreslení pravoúhlého **********");
+    Console.WriteLine("*************** trojúhelníku ****************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Matěj Svoboda ******************");
+    Console.WriteLine("************** 30.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte výšku trojúhelníku (celé číslo): ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku trojúhelníku znovu: ");
+    }
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
