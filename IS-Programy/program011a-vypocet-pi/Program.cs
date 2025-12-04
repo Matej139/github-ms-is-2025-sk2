35a628bda6291902b0ed01d4c56f01161f52493b
Matěj Svoboda <matej.svoboda@tul.cz>
2025-12-04 12:47:57 +0100

    #13 Zakladni program

--- IS-Programy/program011a-vypocet-pi/Program.cs
+++ IS-Programy/program011a-vypocet-pi/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*************** Výpočet PI  ****************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Matěj Svoboda*****************");
+    Console.WriteLine("************** 4.12.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte číslo (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu: ");
+    }
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
